ea6198ec991836fca5e5a610037401a6bdce1d52
Admin <pash.vadim.99@bk.ru>
2020-04-24 15:48:03 +0300

    Доделал страницу авторизации

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
 import './App.css';
 import './fonts/stylesheet.css';
-import {BrowserRouter, Route} from "react-router-dom";
 import Authorization from "./components/ authorization/authorization";
 
 const App = () => {
 
     return (
-        <BrowserRouter>
+        <div>
             <div className="wrapper">
                 <Authorization/>
                 <div className="body">
 
                 </div>
             </div>
-        </BrowserRouter>
+        </div>
     );
 };
 
--- src/components/ authorization/authorization.js
+++ src/components/ authorization/authorization.js
@@ -1,48 +1,41 @@
 import React from "react";
 import './authorization.css';
-import Fon from '../../img/jpg/fon1.jpg'
-import logo from '../../img/png/logo.png'
+import Fon from '../../img/jpg/logoc.jpg'
 import Connect from '../../img/png/connect.png'
-import {NavLink} from "react-router-dom";
+
 
 const Authorization = () => {
     return (
         <div className="mainWindow">
             <div className="Fon"><img src={Fon} alt="/"/></div>
-            <div className="authContent">
                 <div className="banner">
-                    <img src={logo} alt="/"/>
                     <div className="title">
-                        <h1>RAMSIDER</h1>
-                        <h1>ELECTRONIC HOOKAH</h1>
-                        <h1>TECHNO-LUXURY</h1>
+                        <span>ramsider</span> <br/>
+                        <span>electronic hookah</span> <br/>
+                        <span>techno-luxury</span>
                     </div>
                 </div>
                 <div className="authorization">
                     <form action="#">
-                        <div className="formLabel"><span className="titLabel">authorization</span></div>
-                        <div className="login">
-                            <span className="titLabel">login</span>
-                            <label htmlFor="login"> </label>
-                            <input type="text" id="login" placeholder="Enter your username"/>
-                        </div>
-                        <div className="login">
-                            <span className="titLabel">password</span>
-                            <label htmlFor="login"> </label>
-                            <input type="text" id="login" placeholder="Enter your password"/>
-                        </div>
-                        <div className="formButton">
-                            <button>add new user registration</button>
-                            <button>enter</button>
+                        <div className="titAuto"><span>Authorization</span></div>
+                        <div className="formCont">
+                            <div className="titLog"> <span>login</span></div>
+                            <div className="login">
+                                <label htmlFor="login"> </label>
+                                <input type="text" id="login" placeholder="administrator"/>
+                            </div>
+                            <div className="titPass"><span>password</span></div>
+                            <div className="password">
+                                <label htmlFor="password"> </label>
+                                <input type="password" id="pass" placeholder="xxxxxx"/>
+                            </div>
+                            <div className="add"><span>add new user <br/> registration</span><button>enter </button></div>
                         </div>
                     </form>
                 </div>
                 <div className="mainFooter">
                     <img src={Connect} alt="/"/>
-                    <h1>CONNECT</h1>
-                    <span>with hookah</span>
                 </div>
-            </div>
         </div>
     );
 }
